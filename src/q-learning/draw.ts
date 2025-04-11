476171bdb073c330ba9ef01128b971d9634f84ce
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-04-11 11:47:04 +0900

    refactor: separate draw line func

--- src/q-learning/draw.ts
+++ src/q-learning/draw.ts
@@ -1,74 +1,72 @@
-import { CELL_SIZE, idx } from "./main"
+const CELL_SIZE = 80;
 
-const generateCircle = (i: number, j: number, color: string) => {
-  const s = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  s.setAttribute("cx", (CELL_SIZE * (i + 0.5)).toString());
-  s.setAttribute("cy", (CELL_SIZE * (j + 0.5)).toString());
-  s.setAttribute("r", (CELL_SIZE / 4).toString());
+const createSvgElem = <T extends keyof SVGElementTagNameMap>(tag: T): SVGElementTagNameMap[T] => document.createElementNS("http://www.w3.org/2000/svg", tag);
+
+const createLine = (x1: number, y1: number, x2: number, y2: number, color: string, strokeWidth?: number) => {
+  const s = createSvgElem('line')
+  s.setAttribute("x1", String(x1 * CELL_SIZE));
+  s.setAttribute("y1", String(y1 * CELL_SIZE));
+  s.setAttribute("x2", String(x2 * CELL_SIZE));
+  s.setAttribute("y2", String(y2 * CELL_SIZE));
+  s.setAttribute("stroke", color);
+  if (strokeWidth) s.setAttribute("stroke-width", String(strokeWidth));
+  return s;
+};
+
+const createCircle = (i: number, j: number, color: string) => {
+  const s = createSvgElem('circle')
+  s.setAttribute("cx", String(CELL_SIZE * (i + 0.5)));
+  s.setAttribute("cy", String(CELL_SIZE * (j + 0.5)));
+  s.setAttribute("r", String(CELL_SIZE / 4));
   s.setAttribute("fill", color);
   return s;
 };
 
-const generateArrow = (cx: number, cy: number, color: string, deg: number) => {
-  const s = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  s.setAttribute("d", "M -0.2 -1 L 0.2 -1 L 0.2 0 L 0.7 0 L 0 1 L -0.7 0 L -0.2 0 Z");
+const createArrow = (cx: number, cy: number, color: string, deg: number) => {
+  const s = createSvgElem('path')
+  s.setAttribute("d", "M -0.5 -1 L 0 -1 L 0 0.8 L 0.7 0.8 L -0.5 2 Z");
   s.setAttribute("transform", `translate(${CELL_SIZE * (cx + 0.5)} ${CELL_SIZE * (cy + 0.5)}) scale(${CELL_SIZE / 8}) rotate(${deg})`);
   s.setAttribute("fill", color);
   return s;
 };
 
 export const drawGrid = (svgElement: HTMLElement, grid: number[]) => {
-  svgElement.setAttribute("width", (CELL_SIZE * 6).toString());
-  svgElement.setAttribute("height", (CELL_SIZE * 6).toString());
+  // initialize
+  svgElement.setAttribute("width", String(CELL_SIZE * 6));
+  svgElement.setAttribute("height", String(CELL_SIZE * 6));
   svgElement.setAttribute("viewBox", `${-CELL_SIZE / 2} ${-CELL_SIZE / 2} ${CELL_SIZE * 6} ${CELL_SIZE * 6}`);
   svgElement.innerHTML = "";
 
   // grid の分割部分
   for (let i = 0; i <= 5; ++i) {
     for (let j = 0; j <= 5; ++j) {
-      const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-      const yoko = document.createElementNS("http://www.w3.org/2000/svg", "line");
-      yoko.setAttribute("x1", (i * CELL_SIZE).toString());
-      yoko.setAttribute("y1", (Math.max(0, j - 0.1) * CELL_SIZE).toString());
-      yoko.setAttribute("x2", (i * CELL_SIZE).toString());
-      yoko.setAttribute("y2", (Math.min(5, j + 0.1) * CELL_SIZE).toString());
-      yoko.setAttribute("stroke", "black");
+      const group = createSvgElem("g");
+      const yoko = createLine(i, Math.max(0, j - 0.1), i, Math.min(5, j + 0.1), "black");
       group.appendChild(yoko);
-      const tate = document.createElementNS("http://www.w3.org/2000/svg", "line");
-      tate.setAttribute("x1", (Math.max(0, i - 0.1) * CELL_SIZE).toString());
-      tate.setAttribute("y1", (j * CELL_SIZE).toString());
-      tate.setAttribute("x2", (Math.min(5, i + 0.1) * CELL_SIZE).toString());
-      tate.setAttribute("y2", (j * CELL_SIZE).toString());
-      tate.setAttribute("stroke", "black");
+      const tate = createLine(Math.max(0, i - 0.1), j, Math.min(5, i + 0.1), j, "black");
       group.appendChild(tate);
       svgElement.appendChild(group);
     }
   }
 
   // 壁
-  const wall = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  wall.setAttribute("x1", (2 * CELL_SIZE).toString());
-  wall.setAttribute("y1", (1 * CELL_SIZE).toString());
-  wall.setAttribute("x2", (2 * CELL_SIZE).toString());
-  wall.setAttribute("y2", (4 * CELL_SIZE).toString());
-  wall.setAttribute("stroke", "black");
-  wall.setAttribute("stroke-width", "5");
+  const wall = createLine(2, 1, 2, 4, "black", 5);
   svgElement.appendChild(wall);
 
   // S, G
-  svgElement.appendChild(generateCircle(0, 2, "blue"));
-  svgElement.appendChild(generateCircle(4, 0, "red"));
-  svgElement.appendChild(generateCircle(4, 4, "red"));
-  svgElement.appendChild(generateCircle(3, 4, "black"));
+  svgElement.appendChild(createCircle(0, 2, "blue"));
+  svgElement.appendChild(createCircle(4, 0, "red"));
+  svgElement.appendChild(createCircle(4, 4, "red"));
+  svgElement.appendChild(createCircle(3, 4, "black"));
 
   // 矢印
   for (let i = 0; i < 5; ++i) {
     for (let j = 0; j < 5; ++j) {
       const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-      group.appendChild(generateArrow(i - 0.5, j - 0.1, "green", 90));
-      group.appendChild(generateArrow(i + 0.5, j + 0.1, "green", 270));
-      group.appendChild(generateArrow(i - 0.1, j - 0.5, "green", 180));
-      group.appendChild(generateArrow(i + 0.1, j + 0.5, "green", 0));
+      group.appendChild(createArrow(i - 0.5, j + 0.1, "green", 90)); // L
+      group.appendChild(createArrow(i + 0.5, j - 0.1, "green", 270)); // R
+      group.appendChild(createArrow(i - 0.1, j - 0.5, "green", 180)); // U
+      group.appendChild(createArrow(i + 0.1, j + 0.5, "green", 0)); // D
       svgElement.appendChild(group);
     }
   }
--- src/q-learning/main.ts
+++ src/q-learning/main.ts
@@ -3,10 +3,8 @@ import { drawGrid } from "./draw"
 const inputElement = document.getElementById("input");
 const svgElement = document.getElementById("svg");
 
-export const CELL_SIZE = 80;
-
-export const idx = (i: number, j: number) => i * 5 + j;
 
 if (svgElement) {
+  // initialize
   drawGrid(svgElement, new Array(25).fill(0));
 }
