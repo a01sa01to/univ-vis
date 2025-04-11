d3869beadf774c5da932ffa4e3e8d5546506fdc2
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-04-11 12:31:07 +0900

    feat: add sample code

--- src/q-learning/main.ts
+++ src/q-learning/main.ts
@@ -1,13 +1,15 @@
 import { drawGrid } from "./draw"
+import sampleCode from "./sample-code"
 
 const inputElement = document.getElementById("input");
 const svgElement = document.getElementById("svg");
+const sampleCodePre = document.getElementById("sample-code");
 
-
-if (svgElement && inputElement) {
+if (svgElement && inputElement && sampleCodePre) {
   // initialize
   drawGrid(svgElement, new Array(25).fill(100));
 
+  sampleCodePre.innerHTML = sampleCode;
 }
 else {
   alert("正しく読み込まれませんでした。 再読み込みしてみてください。");
