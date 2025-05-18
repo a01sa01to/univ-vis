1d2919f13302a898c8bb515f92f07d0952a4aed5
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-05-18 16:11:54 +0900

    format

--- src/q-learning/main.ts
+++ src/q-learning/main.ts
@@ -1,12 +1,14 @@
-import { drawGrid } from "./draw"
+import { drawGrid } from "./draw";
 
 const inputElement = document.getElementById("input") as HTMLTextAreaElement;
-const svgElement = document.getElementById("svg")
+const svgElement = document.getElementById("svg");
 const errElement = document.getElementById("error");
 const rangeDisplayElement = document.getElementById("round");
 const rangeMaxElement = document.getElementById("round-max");
 const rangeElement = document.getElementById("scale") as HTMLInputElement;
-const showExampleButton = document.getElementById("show-example") as HTMLButtonElement;
+const showExampleButton = document.getElementById(
+  "show-example",
+) as HTMLButtonElement;
 
 if (svgElement) drawGrid(svgElement, new Array(100).fill(0));
 
@@ -14,12 +16,13 @@ let values = new Array(1).fill(new Array(100).fill(0));
 
 const onChange = () => {
   const val = inputElement.value;
-  const round = val.split("\n").filter((v) => v.length > 0)
+  const round = val.split("\n").filter((v) => v.length > 0);
   if (rangeMaxElement) rangeMaxElement.innerHTML = String(round.length);
   if (rangeElement) {
     if (Number(rangeElement.value) > round.length) {
       rangeElement.value = String(round.length);
-      if (rangeDisplayElement) rangeDisplayElement.innerHTML = String(round.length);
+      if (rangeDisplayElement)
+        rangeDisplayElement.innerHTML = String(round.length);
     }
     rangeElement.max = String(round.length);
   }
@@ -28,24 +31,34 @@ const onChange = () => {
   for (let i = 0; i < round.length; ++i) {
     const line = round[i].split(" ").filter((v) => v.length > 0);
     if (line.length !== 100) {
-      if (errElement) errElement.innerHTML = `${i + 1} 行目の要素数が正しくありません。 ${line.length} 個の値があります。 100 個の要素を空白区切りで指定してください。`;
+      if (errElement)
+        errElement.innerHTML = `${i + 1} 行目の要素数が正しくありません。 ${line.length} 個の値があります。 100 個の要素を空白区切りで指定してください。`;
       return;
     }
     for (let j = 0; j < line.length; ++j) {
       const num = Number(line[j]);
       if (Number.isNaN(num)) {
-        if (errElement) errElement.innerHTML = `${i + 1} 行目の ${j + 1} 番目の値 "${line[j]}" は数値ではありません。`;
+        if (errElement)
+          errElement.innerHTML = `${i + 1} 行目の ${j + 1} 番目の値 "${line[j]}" は数値ではありません。`;
         return;
       }
     }
   }
 
-  values = [new Array(100).fill(0), ...round.map((line) => line.split(" ").filter((v) => v.length > 0).map((v) => Number(v)))];
+  values = [
+    new Array(100).fill(0),
+    ...round.map((line) =>
+      line
+        .split(" ")
+        .filter((v) => v.length > 0)
+        .map((v) => Number(v)),
+    ),
+  ];
   if (svgElement) drawGrid(svgElement, values[0]);
-}
+};
 
 if (inputElement) {
-  inputElement.addEventListener("change", onChange)
+  inputElement.addEventListener("change", onChange);
 }
 
 if (rangeElement) {
@@ -55,17 +68,20 @@ if (rangeElement) {
     if (rangeDisplayElement) rangeDisplayElement.innerHTML = val;
     const round = Number(val);
     if (round < 0 || round >= values.length) {
-      if (errElement) errElement.innerHTML = `0 以上 ${values.length - 1} 以下の値を指定してください。`;
+      if (errElement)
+        errElement.innerHTML = `0 以上 ${values.length - 1} 以下の値を指定してください。`;
       return;
     }
     if (svgElement) drawGrid(svgElement, values[round]);
-  })
+  });
 }
 
 if (showExampleButton) {
   showExampleButton.addEventListener("click", async () => {
-    const sample = await fetch("/univ-vis/qlearning-example.txt").then((res) => res.text());
+    const sample = await fetch("/univ-vis/qlearning-example.txt").then((res) =>
+      res.text(),
+    );
     if (inputElement) inputElement.value = sample;
     onChange();
-  })
+  });
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { resolve } from "node:path"
-import { defineConfig } from "vite"
-import scssPlugin from "@csstools/postcss-sass"
-import legacyPnpImporter from "pnp-sass-importer/legacy"
-import scss from "postcss-scss"
+import { resolve } from "node:path";
+import scssPlugin from "@csstools/postcss-sass";
+import legacyPnpImporter from "pnp-sass-importer/legacy";
+import scss from "postcss-scss";
+import { defineConfig } from "vite";
 
-const srcDir = resolve(__dirname, "src")
-const distDir = resolve(__dirname, "dist")
-const publicDir = resolve(__dirname, "public")
+const srcDir = resolve(__dirname, "src");
+const distDir = resolve(__dirname, "dist");
+const publicDir = resolve(__dirname, "public");
 
 export default defineConfig({
   root: srcDir,
@@ -17,20 +17,20 @@ export default defineConfig({
     outDir: distDir,
     rollupOptions: {
       input: {
-        'particle-filter': resolve(srcDir, "particle-filter", "index.html"),
-        'q-learning': resolve(srcDir, "q-learning", "index.html"),
-        '.': resolve(srcDir, "index.html")
-      }
-    }
+        "particle-filter": resolve(srcDir, "particle-filter", "index.html"),
+        "q-learning": resolve(srcDir, "q-learning", "index.html"),
+        ".": resolve(srcDir, "index.html"),
+      },
+    },
   },
   css: {
     postcss: {
       syntax: scss,
       plugins: [
         scssPlugin({
-          importer: legacyPnpImporter(__dirname)
-        })
-      ]
-    }
-  }
-})
+          importer: legacyPnpImporter(__dirname),
+        }),
+      ],
+    },
+  },
+});
